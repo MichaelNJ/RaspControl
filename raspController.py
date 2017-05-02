ae53de256d3281c29fd50fe7902a7f8e7bb9ebe4
MichaelNJ <kamaumichael90@gmail.com>
2017-05-02 08:55:01 +0300

    Good COmmit

--- raspController.py
+++ raspController.py
@@ -26,11 +26,11 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
         root = parse.getroot()
         stored = root[0][0].text
 
-        states = []
+        self.states = []
 
         if len(root) == 5:
             for i in range(len(root)):
-                states.append(root[i][0].text)
+                self.states.append(root[i][0].text)
                 i += 1
         else:
             self.stackedWidget.setCurrentIndex(3)
@@ -55,7 +55,7 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
         self.goback.clicked.connect(lambda : self.errorBack())
 
         # Login
-        self.login.clicked.connect(lambda : self.logIn(root, states))
+        self.login.clicked.connect(lambda : self.logIn(root))
 
         self.clearpass.clicked.connect(lambda :  self.clearText())
         self.passEdit.setEnabled(False)
@@ -65,7 +65,15 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
 
         # main window ops
         self.lock_2.clicked.connect(lambda : self.lockBack())
+
+        self.startSecs = 0
+        self.startmins = 0
         self.timer = QtCore.QTimer(self)
+
+        # this timer will control the lcd display, i stop it to start it when i need it
+        self.timer.timeout.connect(self.updateLCD)
+        self.timer.start(1000)
+        self.timer.stop()
 
         self.start.clicked.connect(lambda : self.startBiz())
         # self.worker = WorkerThread()
@@ -82,9 +90,10 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
         print str(len(current)) + index
         self.passEdit.setText(current + index)
 
-    def logIn(self, root, states):
+    def logIn(self, root):
         self.lcdnum.display("00"+ ":"+ "00")
         current = self.passEdit.text()
+
         if not len(current) > 0:
             self.passEdit.setStyleSheet("border-style: solid;	border-color: rgb(255, 0, 0); border-width:2px; border-radius: 7px;")
 
@@ -95,14 +104,14 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
             self.passEdit.setText("")
             self.passEdit.setStyleSheet("border-style: solid;	border-color: rgb(255, 0, 0); border-width:2px; border-radius: 7px;")
         
-        if states[1] == "1":
+        """if states[2] == "1":
             self.rone_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: outset; border-radius: 6px;")
 
-        if states[2] == "1":
+        if states[3] == "1":
             self.rtwo_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: outset; border-radius: 6px;")
 
-        if states[3] == "1":
-            self.rthree_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: outset; border-radius: 6px;")
+        if states[4] == "1":
+            self.rthree_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: outset; border-radius: 6px;")"""
 
     def clearText(self):
         self.passEdit.clear()
@@ -113,15 +122,55 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
         self.stackedWidget.setCurrentIndex(1)
 
     def updateLCD(self):
-        
-        mins = "00"
-        secs = "00"
         self.startSecs += 1
-        self.lcdnum.display("00 :" + "%s" %self.startSecs )
-        print "LCD updating %s " % self.startSecs
-        if self.startSecs == 10:
-            self.lcdnum.display("01 :" + "0%s" % (self.startSecs-10) )
+        self.updateDet()
+
+
+    def updateDet(self):
+        self.lcdnum.display("0"+"%s" % str(self.startmins) + ":" + "%s" % str(self.startSecs))
+
+        if self.startSecs == 60:
+            self.startSecs = 0
+            self.startmins += 1
+            self.lcdnum.display("0"+"%s" % str(self.startmins) + ":" + "%s" % str(self.startSecs))
+
+        if self.startSecs <= 9:
+            self.lcdnum.display("0"+"%s" % str(self.startmins) + ":0" + "%s" % str(self.startSecs))
+
+        if self.startmins == 4:
+            self.lcdnum.display("0"+"%s" % str(self.startmins) + ":0" + "%s" % str(self.startSecs))
             self.timer.stop()
+
+        # manipulate the relays and update xml file
+
+        # at three minutes, turn relay one on, turn relay 2 on
+        if self.startmins == 3 and self.startSecs == 0:
+            # relay one on
+            self.rone_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
+
+            # relay two on
+            self.rtwo_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
+        
+        # at 3 mins and 35 seconds turn relay 2 off
+        if self.startmins == 3 and self.startSecs == 5:
+            # relay 2 off
+            print self.states[3]
+            self.rtwo_2.setStyleSheet("background-color: rgb(255, 255, 255); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
+
+
+        # at 3 mins and 30 seconds turn relay one off, relay 3 on 
+        if self.startmins == 3 and self.startSecs == 30:
+            # relay one off
+            self.rone_2.setStyleSheet("background-color: rgb(255, 255, 255); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
+
+            # relay 3 on
+            print self.states[3]
+            self.rthree_2.setStyleSheet("background-color: rgb(85, 255, 127); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
+
+
+        # at 3 mins and 35 seconds turn relay 3 off
+        if self.startmins == 3 and self.startSecs == 35:
+            self.rthree_2.setStyleSheet("background-color: rgb(255, 255, 255); border-style: solid; border-radius: 4px; border-width: 2px; border-color: rgb(180, 180, 180);")
 
 
     def startBiz(self):
@@ -137,6 +186,10 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
 
             # 0 is START 1 is STOP
             if root[1][0].text == "0":
+
+                # Stop (Pause) the timer
+                self.timer.stop()
+
                 #kill all background music when stop button is pressed
                 subprocess.Popen(['killall', 'mpg321'])
                 
@@ -145,16 +198,19 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
                 self.start.setText("START")
                 root[1][0].text = str(1)
                 root.set('Toggle', 'On/Off')
+
+                # revert the timer back to zero and display it on the lcd
+                #self.startSecs = 0
+                #self.lcdnum.display("00"+":"+"00")
             
             elif root[1][0].text == "1":
-                if numMps == len(mp3_files):
+                if len(mp3_files) == numMps:
                     self.stackedWidget.setCurrentIndex(3)
                     self.errorOccur.setText("An Error Occurred")
                     self.errorCode.setText("<b>Error Code</b>" + " :550")
                     self.realError.setText("Did not find any .mp3 files in the Musics Folder")
                 else:
                     x = random.randint(0, len(mp3_files))
-                    print x
                     try:
                         subprocess.Popen(['mpg321', './MusicFiles/%s' % mp3_files[x]])
                         print "Playing mp3 file "
@@ -163,16 +219,16 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
                         subprocess.Popen(['mpg321', './MusicFiles/%s' % mp3_files[x-1]])
                         print "Playing mp3 file "
                         time.sleep(1)
-                    
 
                 self.start.setText("STOP")
                 self.start.setStyleSheet("background-color: rgb(255, 87, 90); border-style: solid; border-radius: 7px; border-width: 3px; border-color: rgb(180, 180, 180);")
                 root[1][0].text = str(0)
                 root.set('Toggle', 'On/Off')
 
-            else:
-            
+                # Start (Continue) with thhe timer
+                self.timer.start()
 
+            else:
                 self.stackedWidget.setCurrentIndex(3)
                 self.errorOccur.setText("An Error Occurred")
                 self.errorCode.setText("<b>Error Code</b>" + " :450")
@@ -186,9 +242,6 @@ class controller(QtGui.QMainWindow, rasp_controlsFinal.Ui_MainWindow):
             self.realError.setText("Did not find any .mp3 files in the Musics Folder")
         else:
             print "There are %s .mp3 files in the MusicFiles Directory" % len(mp3_files)
-            
-        
-        #self.start.setStyleSheet("")
         
     
 def main():
